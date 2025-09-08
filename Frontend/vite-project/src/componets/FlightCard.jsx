71fcecc6bcd5dd30da78afdb5cf1b51961365a5a
manishrajora25 <manishrajora453@gmail.com>
2025-09-08 22:57:24 +0530

    settheader

--- Frontend/vite-project/src/componets/FlightCard.jsx
+++ Frontend/vite-project/src/componets/FlightCard.jsx
@@ -1,12 +1,1 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-const FlightCard = props => {
-  return (
-    <div>FlightCard</div>
-  )
-}
-
-FlightCard.propTypes = {}
-
-export default FlightCard
+r
