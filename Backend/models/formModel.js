d0efa22ddc6305f884f939898a7665b3a74e8908
manishrajora25 <manishrajora453@gmail.com>
2025-09-10 22:11:18 +0530

    awsedr

--- Backend/models/formModel.js
+++ Backend/models/formModel.js
@@ -30,30 +30,3 @@ export default Form;
 
 
 
-
-
-// // models/formModel.js
-// import mongoose from "mongoose";
-
-// const formSchema = new mongoose.Schema(
-//   {
-//     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // NEW: who booked
-//     name: { type: String },
-//     email: { type: String },
-//     phone: { type: String },
-//     from: { type: String, required: true },
-//     to: { type: String, required: true },
-//     departureDate: { type: String, required: true },
-//     returnDate: { type: String },
-//     passengers: { type: Number, required: true },
-//     travelClass: {
-//       type: String,
-//       enum: ["ECONOMY", "PREMIUM_ECONOMY", "BUSINESS", "FIRST"],
-//       default: "ECONOMY",
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// const Form = mongoose.model("Form", formSchema);
-// export default Form;
