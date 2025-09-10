c4f3932bcf6332a3569c29813193a5deaff6e855
manishrajora25 <manishrajora453@gmail.com>
2025-09-10 22:00:22 +0530

    changeform

--- Backend/routes/formRoute.js
+++ Backend/routes/formRoute.js
@@ -1,15 +1,5 @@
 
 
-// import express from "express";
-// import { addForm, getForms } from "../controllers/formController.js";
-
-// const router = express.Router();
-
-// router.post("/add", addForm);
-// router.get("/", getForms);
-
-// export default router;
-
 
 
 
